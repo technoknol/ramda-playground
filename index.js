a737b1c48646c9dbc8ae73783acb316ff2f94fe1
technoknol <6429418+technoknol@users.noreply.github.com>
2023-01-31 19:43:40 +0530

    CH8-Releasing the Value - add little helper

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ import M  from 'ramda-fantasy';
 const Maybe = M.Maybe
 const Just = Maybe.Just;
 const Nothing = Maybe.Nothing;
-// const { compose,  curry , map } = require('ramda');
+console.clear(); 
 
 // withdraw :: Number -> Account -> Maybe(Account)
 // const withdraw = curry((amount, { balance }) =>
 //   Maybe.of(balance >= amount ? { balance: balance - amount } : Nothing()));
 
-  const withdraw = curry((amount, { balance }) =>
+// maybe :: b -> (a -> b) -> Maybe a -> b
+const maybe = curry((v, f, m) => {
+  console.log('m',{m, v , f} )
+  if (m.isNothing) {
+    return v;
+  }
+
+  return f(m.value);
+});
+
+const withdraw = curry((amount, { balance }) =>
   balance >= amount ? Maybe.of({ balance: balance - amount }) : Nothing());
 
 // This function is hypothetical, not implemented here... nor anywhere else.
@@ -26,6 +36,10 @@ const finishTransaction = compose(remainingBalance, updateLedger);
 // getTwenty :: Account -> Maybe(String)
 const getTwenty = compose(map(finishTransaction), withdraw(20));
 
+// getTwenty :: Account -> String
+const getTwenty2 = compose(maybe('You\'re broke!', finishTransaction), withdraw(20));
+
+
 const r = getTwenty({ balance: 200.00 });  // Just('Your balance is $180')
 console.log({r});
 
@@ -33,4 +47,6 @@ console.log({r});
 const t = getTwenty({ balance: 0 }); // Nothing
 console.log({t});;
 
-// console.log('afasfasfa', withdraw(20)({balance: 0})) 
+
+const t2 = getTwenty2({ balance: 0 }); // "You're broke!" 
+console.log({t2});
