4be612aa39df9cc898fd2abfcc74042b631509c6
technoknol <6429418+technoknol@users.noreply.github.com>
2023-01-31 19:21:54 +0530

    fix withdraw function.

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const Nothing = Maybe.Nothing;
 // const { compose,  curry , map } = require('ramda');
 
 // withdraw :: Number -> Account -> Maybe(Account)
-const withdraw = curry((amount, { balance }) =>
-  Maybe.of(balance >= amount ? { balance: balance - amount } : null));
+// const withdraw = curry((amount, { balance }) =>
+//   Maybe.of(balance >= amount ? { balance: balance - amount } : Nothing()));
+
+  const withdraw = curry((amount, { balance }) =>
+  balance >= amount ? Maybe.of({ balance: balance - amount }) : Nothing());
 
 // This function is hypothetical, not implemented here... nor anywhere else.
 // updateLedger :: Account -> Account 
@@ -23,10 +26,11 @@ const finishTransaction = compose(remainingBalance, updateLedger);
 // getTwenty :: Account -> Maybe(String)
 const getTwenty = compose(map(finishTransaction), withdraw(20));
 
-const r = getTwenty({ balance: 200.00 }); 
+const r = getTwenty({ balance: 200.00 });  // Just('Your balance is $180')
 console.log({r});
-// Just('Your balance is $180')
 
-const t = getTwenty({ balance: 10.00 });
-console.log({t});
-// Nothing
+
+const t = getTwenty({ balance: 0 }); // Nothing
+console.log({t});;
+
+// console.log('afasfasfa', withdraw(20)({balance: 0})) 
