9adad7d311f24cb7e607ccdd92710a94f4ffaf2d
technoknol <6429418+technoknol@users.noreply.github.com>
2023-01-31 21:45:25 +0530

    Add lib, refactor to multiple files, add Either from CH8, add moment.

---
+++ Either.js
@@ -0,0 +1,38 @@
+const moment = require('moment');
+
+import { Either, Left, prop,  startLog, endLog } from 'lib'
+import { curry } from 'ramda';
+
+startLog('either')
+
+const left = x => new Left(x);
+
+const brain = Either.of('rain').map(str => `b${str}`); // Right('brain')
+console.log(brain);
+
+const leftrain = left('rain').map(str => `It's gonna ${str}, better bring your umbrella!`); // Left('rain')
+console.log(leftrain);
+
+const localhost = Either.of({ host: 'localhost', port: 80 }).map(prop('host')); // Right('localhost')
+console.log(localhost)
+
+const rolleyes = left('rolls eyes...').map(prop('host')); // Left('rolls eyes...');
+console.log(rolleyes );
+
+// getAge :: Date -> User -> Either(String, Number)
+const getAge = curry((now, user) => {
+    const birthDate = moment(user.birthDate, 'YYYY-MM-DD');
+
+    return birthDate.isValid() 
+    ? Either.of(now.diff(birthDate, 'years'))
+    : left('Birthdate could not be parsed');
+})
+
+const validAge = getAge(moment(), { birthDate: '1992-07-18'});
+console.log(validAge);
+
+const inValidAge = getAge(moment(), { birthDate: 'July 4 21'});
+console.log(inValidAge);
+
+
+endLog('either')
--- index.js
+++ index.js
@@ -1,51 +1,6 @@
-import { curry, map, compose, prop }  from 'ramda'
-import M  from 'ramda-fantasy';
-const Maybe = M.Maybe
-const Just = Maybe.Just;
-const Nothing = Maybe.Nothing;
 console.clear(); 
+import '/lib/index'
+import './Maybe'
+import './Either'
 
-// withdraw :: Number -> Account -> Maybe(Account)
-// const withdraw = curry((amount, { balance }) =>
-//   Maybe.of(balance >= amount ? { balance: balance - amount } : Nothing()));
-
-// maybe :: b -> (a -> b) -> Maybe a -> b
-const maybe = curry((v, f, m) => {
-  if (m.isNothing) {
-    return v;
-  }
-
-  return f(m.value);
-});
-
-const withdraw = curry((amount, { balance }) =>
-  balance >= amount ? Maybe.of({ balance: balance - amount }) : Nothing());
-
-// This function is hypothetical, not implemented here... nor anywhere else.
-// updateLedger :: Account -> Account 
-const updateLedger = account => account;
-
-// remainingBalance :: Account -> String
-// const remainingBalance = (account) => `Your balance is $${prop(`balance`)(account)}`;
-const remainingBalance = ({ balance }) => `Your balance is $${balance}`;
-
-// finishTransaction :: Account -> String
-const finishTransaction = compose(remainingBalance, updateLedger);
-
-// getTwenty :: Account -> Maybe(String)
-const getTwenty = compose(map(finishTransaction), withdraw(20));
-
-// getTwenty :: Account -> String
-const getTwenty2 = compose(maybe('You\'re broke!', finishTransaction), withdraw(20));
-
-
-const r = getTwenty({ balance: 200.00 });  // Just('Your balance is $180')
-console.log({r});
-
-
-const t = getTwenty({ balance: 0 }); // Nothing
-console.log({t});;
-
-
-const t2 = getTwenty2({ balance: 0 }); // "You're broke!" 
-console.log({t2});
+// console.log('maybe',maybe)
